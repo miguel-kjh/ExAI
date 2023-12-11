956a60d49328cb089462f123af0aa65acf96421e
Miguel-kjh <miguelangel.kjh@gmial.com>
2023-12-11 22:54:23 +0000

    update

--- experiments/utils.py
+++ experiments/utils.py
@@ -2,6 +2,11 @@ import os
 from torch.utils.data import DataLoader, Dataset, random_split
 from torchvision import datasets, transforms
 from torch.utils.data.dataset import Subset
+# get confusion matrix
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 
@@ -71,4 +76,58 @@ def find_low_activation_neurons(df, number1, number2, threshold=0.5):
 
     # 5. Retornar la lista de neuronas en la intersección
     return list(intersection_neurons)
+
+
+
+def plot_confusion_matrix(y_true, y_pred, classes, normalize=False, title=None, cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    Source: https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    #classes = classes[unique_labels(y_true, y_pred)]
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           #xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+
+
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    fontsize=20,
+                    color="white" if cm[i, j] > thresh else "black")
+
+
+    fig.tight_layout()
+    return ax
 
