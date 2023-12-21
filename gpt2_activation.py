3d33b65836d1e20fb30da7d8c3503422f4e9f1cc
miguel-kjh <miguelangel.kjh@gmail.com>
2023-12-21 12:20:20 +0000

    refactor gpt2 activations recollection

--- gpt2_activation.py
+++ gpt2_activation.py
@@ -1,63 +1,47 @@
 import pickle
+import os
 import pandas as pd
 import torch
 from tqdm import tqdm
 from transformers import GPT2Tokenizer, GPT2Config
-from transformers.models.gpt2.modeling_gpt2 import GPT2Attention
-import numpy as np
-
-import networkx as nx
-import matplotlib.pyplot as plt
-
-# no warnings
-import warnings
 from LLMHeadModelWithFFNOutput import LLMHeadModelWithFFNOutput
 
-warnings.filterwarnings("ignore")
 
+# Configuración del modelo y el tokenizer
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 config = GPT2Config.from_pretrained('gpt2')
 num_layers = config.n_layer
-num_neurons = config.hidden_size*4
+num_neurons = config.hidden_size * 4
+max_length = 2
+folder = os.path.join('experiments', "activations")
+file = os.path.join(folder, "gpt2.pickle")
 model = LLMHeadModelWithFFNOutput(config)
-
-
-# Asegúrate de que el modelo esté en modo de evaluación
 model.eval()
-
-# Asegúrate de que las operaciones se realicen en el dispositivo correcto
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
+# Datos de entrada
 text = ["Obama ", "Donal ", "Trump ", "Joe "]
 
-columns = ["Input_text", "Output_text"]
-for layer_index in range(num_layers):
-    columns += [f'layer_{layer_index+1}_Neuron{i+1}' for i in range(num_neurons)]
+# Preparar columnas para el DataFrame
+columns = ["Input_text", "Output_text"] + [f'layer_{i+1}_Neuron{j+1}' for i in range(num_layers) for j in range(num_neurons)]
+rows = []
 
-df = pd.DataFrame(index=range(len(text)), columns=columns)
-                  
-for t in tqdm(text, desc="Texts"): 
+# Procesamiento de textos
+for t in tqdm(text, desc="Texts"):
     input_ids = torch.tensor([tokenizer.encode(t)]).to(device)
-    #tokens = tokenizer.tokenize(t)
-
-    outputs_tokens = model.generate(input_ids, max_length=2, pad_token_id=tokenizer.eos_token_id)
+    outputs_tokens = model.generate(input_ids, max_length=max_length, pad_token_id=tokenizer.eos_token_id)
     output_text = tokenizer.decode(outputs_tokens[0], skip_special_tokens=True)
-
     activation_ffn = model.get_activation_ffn()
-    
-    row = {
-        "Input_text": t,
-        "Output_text": output_text
-    }
 
+    row = {"Input_text": t, "Output_text": output_text}
     for layer_index in range(num_layers):
         for neuron_index in range(num_neurons):
             row[f'layer_{layer_index+1}_Neuron{neuron_index+1}'] = activation_ffn[layer_index][0][0][neuron_index]
+    rows.append(row)
 
-    df.loc[len(df)] = row
-
-
-#save in pickle
-with open('recollection.pickle', 'wb') as handle:
+# Crear DataFrame y guardar en formato pickle
+df = pd.DataFrame(rows, columns=columns)
+with open(file, 'wb') as handle:
     pickle.dump(df, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
